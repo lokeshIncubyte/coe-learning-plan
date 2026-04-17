3fc6fdd10f7aa1219505fae0238d55af7198039d
lokeshIncubyte <lokesh@incubyte.co>
2026-04-17 18:23:24 +0530

    [day1/string-calculator] GREEN: support multiple numbers

--- day1/string-calculator/calculator.py
+++ day1/string-calculator/calculator.py
@@ -2,6 +2,4 @@ def add(numbers):
     """Add numbers from string. Empty string returns 0."""
     if not numbers:
         return 0
-    if "," in numbers:
-        return sum(int(x) for x in numbers.split(","))
-    return int(numbers)
+    return sum(int(x) for x in numbers.split(","))
