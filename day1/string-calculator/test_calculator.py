7d0fc3ae6b204716ad68d28cbee5d6f4380564d3
lokeshIncubyte <lokesh@incubyte.co>
2026-04-17 18:20:50 +0530

    [day1/string-calculator] RED: empty string should return 0

--- day1/string-calculator/test_calculator.py
+++ day1/string-calculator/test_calculator.py
@@ -1,1 +1,7 @@
-# Empty file - will fill in next
+import pytest
+from calculator import add
+
+class TestStringCalculator:
+    def test_empty_string_returns_zero(self):
+        """Empty string should return 0"""
+        assert add("") == 0
