4d08093ae8ada2b4f2881f28ba708798ded5b0f2
lokeshIncubyte <lokesh@incubyte.co>
2026-04-17 18:21:48 +0530

    [day1/string-calculator] RED: single number should return itself

--- day1/string-calculator/test_calculator.py
+++ day1/string-calculator/test_calculator.py
@@ -5,3 +5,7 @@ class TestStringCalculator:
     def test_empty_string_returns_zero(self):
         """Empty string should return 0"""
         assert add("") == 0
+
+    def test_single_number_returns_value(self):
+        """Single number string should return that number"""
+        assert add("1") == 1
